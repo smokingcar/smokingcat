c0b4860819434e7898b14012945ab2d733ca0a48
smokingproject <smokingcats25@gmail.com>
2024-05-26 19:18:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Smoking Cat Meme Coin website loaded!");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contents = document.querySelectorAll(".contens");
+
+  const observerOptions = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // Stop observing after it's visible
+      }
+    });
+  }, observerOptions);
+
+  contents.forEach((content) => {
+    observer.observe(content);
+  });
+});
